d675bc44294e62eaf07783ebb863dc458faa530d
Petr Fenster <77027099+petrfenster@users.noreply.github.com>
2022-01-15 15:11:35 -0800

    Added if name = main

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+import speech_recognition as sr
 import pyttsx3
-from decouple import config
+import datetime
 
-USERNAME = config('USER')
-BOTNAME = config('BOTNAME')
 
-# Microsoft Speech API 5.4 (sapi5)
-engine = pyttsx3.init('sapi5')
 
-# Set Rate
-engine.setProperty('rate', 190)
+print('Loading your AI personal assistant - G One')
 
-# Set Volume
-engine.setProperty('volume', 1.0)
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
 
-# Set Voice (Male)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+speak("Loading your AI personal assistant G-One")
+wishMe()
+
+if __name__=='__main__':
+    print("All done")
