632081bc056353d64eac0ba62d1eb05f49935daf
Shaon Talukdar <shaontal@students.cs.ubc.ca>
2022-01-16 11:17:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,50 +4,62 @@ import datetime
 import wikipedia
 import requests
 import json
+import matplotlib.pyplot as plt
 from newsapi.newsapi_client import NewsApiClient
+<<<<<<< Updated upstream
 newsapi_p = NewsApiClient(api_key='bd315eb3ac20474a8a27b842ac2fe371')
 
 
 
+=======
+
+newsapi_p = NewsApiClient(api_key='bd315eb3ac20474a8a27b842ac2fe371')
+>>>>>>> Stashed changes
 
 print('Loading your financial personal assistant - G One')
 
-engine=pyttsx3.init()
-voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
 engine.setProperty('volume', 1.0)
 engine.setProperty('rate', 190)
 yahoofinanceurlquotes = "https://yfapi.net/v6/finance/quote"
+<<<<<<< HEAD
+yahoofinanceurloptionchain = "https://yfapi.net/v6/finance/options"
+=======
+yahoofinanceurlchart = "https://yfapi.net/v8/finance/chart/AAPL"
+>>>>>>> 11b12a2e351de09a97bff77fb8f050e04ee0791d
 yahoofinanceapikey = {
     'x-api-key': "DJf9eRcBoZ5yXM7FfFMM47n4ThCajZQQ7iB3MjEU"
-    }
-
+}
 
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
         speak("Hello,Good Morning")
         print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Hello,Good Afternoon")
         print("Hello,Good Afternoon")
     else:
         speak("Hello,Good Evening")
         print("Hello,Good Evening")
 
+
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        audio=r.listen(source)
+        audio = r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='en-in')
+            statement = r.recognize_google(audio, language='en-in')
             print(f"user said:{statement}\n")
 
         except Exception as e:
@@ -55,10 +67,11 @@ def takeCommand():
             return "None"
         return statement
 
+
 speak("Loading your financial personal assistant G-One")
 wishMe()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print("All done")
 
     while True:
@@ -72,10 +85,10 @@ if __name__=='__main__':
             speak('your personal assistant G-one is shutting down,Good bye')
             print('your personal assistant G-one is shutting down,Good bye')
             break
-            
+
         if 'explain' in statement:
             speak('Searching...')
-            statement =statement.replace("explain", "")
+            statement = statement.replace("explain", "")
             results = wikipedia.summary(statement, sentences=3)
             speak("According to Wikipedia")
             print(results)
@@ -85,10 +98,12 @@ if __name__=='__main__':
         if 'company information' in statement:
             speak('Say Company Name')
             company_name = takeCommand().upper()
-            querystring = {"symbols":company_name}
-            speak('Say information needed, options are Quotes, Market Summary, Option Chain information ') #work on summary
+            querystring = {"symbols": company_name}
+            speak(
+                'Say information needed, options are Quotes, Market Summary, Option Chain information ')  # work on summary
             information_parameter = takeCommand().lower()
             if 'quotes' in information_parameter:
+<<<<<<< Updated upstream
 
                 response_json = requests.request("GET", yahoofinanceurlquotes, headers=yahoofinanceapikey, params=querystring)
                 response_dict = json.loads(response_json)
@@ -98,22 +113,90 @@ if __name__=='__main__':
                 continue
             if 'market summary' in information_parameter:
                 continue
-            if 'option chain information' in information_parameter:
+=======
+                response = requests.request("GET", yahoofinanceurlquotes, headers=yahoofinanceapikey,
+                                            params=querystring)
+                response_json = json.loads(response.text)
+                print(response_json["quoteResponse"]["result"][0]['bid'])
+                speak(str(response_json["quoteResponse"]["result"][0]['bid']))
                 continue
+
+>>>>>>> Stashed changes
+            if 'option chain information' in information_parameter:
+
+                response = requests.request("GET", yahoofinanceurloptionchain, headers=yahoofinanceapikey, params=querystring)
+                response_json = json.loads(response.text)
+                print(response_json["quoteResponse"]["result"][0]['bid'])
+                speak(str(response_json["quoteResponse"]["result"][0]['bid']))
+
+                continue
+
             if 'market cap' in information_parameter:
+                response = requests.request("GET", yahoofinanceurlquotes, headers=yahoofinanceapikey,
+                                            params=querystring)
+                response_json = json.loads(response.text)
+                print(response_json["quoteResponse"]["result"][0]['marketCap'])
+                speak(str(response_json["quoteResponse"]["result"][0]['marketCap']))
+                continue
+            if 'chart' in information_parameter:
+                speak('Choose one of the following charts: 5 days, 1 month, 6 months, 1 year, YTD, 5 years')
+                time_parameter = takeCommand().lower()
+                if 'days' in time_parameter:
+                    querystring = {"range": "5d",
+                                   "interval": "15m"}
+                if '1 month' in time_parameter:
+                    querystring = {"range": "1mo",
+                                   "interval": "15m"}
+                if '6' in time_parameter:
+                    querystring = {"range": "6mo",
+                                   "interval": "1d"}
+                if '1 year' in time_parameter:
+                    querystring = {"range": "1y",
+                                   "interval": "1d"}
+                if '5 years' in time_parameter:
+                    querystring = {"range": "5y",
+                                   "interval": "1wk"}
+
+                if 'ytd' in time_parameter:
+                    querystring = {"range": "ytd",
+                                   "interval": "5d"}
+
+                response = requests.request("GET", yahoofinanceurlchart, headers=yahoofinanceapikey,
+                                            params=querystring)
+                response_json = json.loads(response.text)
+                dates_json = response_json['chart']['result'][0]['timestamp']
+                valuations = response_json['chart']['result'][0]['indicators']['quote'][0]['close']
+
+                def to_date(ts):
+                    return datetime.datetime.fromtimestamp(ts)
+
+
+                dates = list(map(to_date, dates_json))
+                plt.plot(dates, valuations)
+                ax = plt.gca()
+                ax.axes.xaxis.set_visible(False)
+                plt.show()
+
                 continue
 
         if 'news' in statement:
             speak('Searching news...')
             statement = statement.replace("news", "")
             top_headlines = newsapi_p.get_top_headlines(q=statement,
+<<<<<<< Updated upstream
                                                       category='business',
                                                       language='en')
             print(top_headlines)
             if top_headlines['totalResults'] > 4:
+=======
+                                                        category='business',
+                                                        language='en')
+            if top_headlines['totalResults'] >= 3:
+>>>>>>> Stashed changes
                 for news_id in range(0, 3):
                     news = top_headlines['totalResults'][news_id]
                     print(news['title'])
+<<<<<<< Updated upstream
 
 
 
@@ -121,3 +204,6 @@ if __name__=='__main__':
 
 
 
+=======
+                    speak(news['title'])
+>>>>>>> Stashed changes
