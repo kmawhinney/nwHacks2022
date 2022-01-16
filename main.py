fde622ee752bb48759c8e593700958784860d801
Petr Fenster <77027099+petrfenster@users.noreply.github.com>
2022-01-16 11:02:17 -0800

    added quotes

--- main.py
+++ main.py
@@ -4,51 +4,59 @@ import datetime
 import wikipedia
 import requests
 import json
+import matplotlib.pyplot as plt
 from newsapi.newsapi_client import NewsApiClient
+
 newsapi_p = NewsApiClient(api_key='bd315eb3ac20474a8a27b842ac2fe371')
-
-
-
-
-
 
 print('Loading your financial personal assistant - G One')
 
-engine=pyttsx3.init()
-voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
 engine.setProperty('volume', 1.0)
 engine.setProperty('rate', 190)
 yahoofinanceurlquotes = "https://yfapi.net/v6/finance/quote"
+yahoofinanceurlchart = "https://yfapi.net/v8/finance/chart/AAPL"
 yahoofinanceapikey = {
     'x-api-key': "DJf9eRcBoZ5yXM7FfFMM47n4ThCajZQQ7iB3MjEU"
-    }
+}
 
+def to_date(ts):
+    return datetime.datetime.fromtimestamp(ts)
+
+dates = list(map(to_date, dates_json))
+plt.plot(dates, valuations)
+ax = plt.gca()
+ax.axes.xaxis.set_visible(False)
+plt.show()
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
         speak("Hello,Good Morning")
         print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Hello,Good Afternoon")
         print("Hello,Good Afternoon")
     else:
         speak("Hello,Good Evening")
         print("Hello,Good Evening")
 
+
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        audio=r.listen(source)
+        audio = r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='en-in')
+            statement = r.recognize_google(audio, language='en-in')
             print(f"user said:{statement}\n")
 
         except Exception as e:
@@ -56,10 +64,11 @@ def takeCommand():
             return "None"
         return statement
 
+
 speak("Loading your financial personal assistant G-One")
 wishMe()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print("All done")
 
     while True:
@@ -74,10 +83,10 @@ if __name__=='__main__':
             speak('your personal assistant G-one is shutting down,Good bye')
             print('your personal assistant G-one is shutting down,Good bye')
             break
-            
+
         if 'explain' in statement:
             speak('Searching...')
-            statement =statement.replace("explain", "")
+            statement = statement.replace("explain", "")
             results = wikipedia.summary(statement, sentences=3)
             speak("According to Wikipedia")
             print(results)
@@ -87,12 +96,13 @@ if __name__=='__main__':
         if 'company information' in statement:
             speak('Say Company Name')
             company_name = takeCommand().upper()
-            querystring = {"symbols":company_name}
-            speak('Say information needed, options are Quotes, Market Summary, Option Chain information ') #work on summary
+            querystring = {"symbols": company_name}
+            speak(
+                'Say information needed, options are Quotes, Market Summary, Option Chain information ')  # work on summary
             information_parameter = takeCommand().lower()
             if 'quotes' in information_parameter:
-
-                response = requests.request("GET", yahoofinanceurlquotes, headers=yahoofinanceapikey, params=querystring)
+                response = requests.request("GET", yahoofinanceurlquotes, headers=yahoofinanceapikey,
+                                            params=querystring)
                 response_json = json.loads(response.text)
                 print(response_json["quoteResponse"]["result"][0]['bid'])
                 speak(str(response_json["quoteResponse"]["result"][0]['bid']))
@@ -104,13 +114,53 @@ if __name__=='__main__':
                 continue
             if 'market cap' in information_parameter:
                 continue
+            if 'chart' in information_parameter:
+                speak('Choose one of the following graphs: 5 days, 1 month, 6 months, 1 year, YTD, 5 years')
+                time_parameter = takeCommand().lower()
+                if 'days' in time_parameter:
+                    querystring = {"range": "5d",
+                                   "interval": "15m"}
+                if '1 month' in time_parameter:
+                    querystring = {"range": "1mo",
+                                   "interval": "15m"}
+                if '6' in time_parameter:
+                    querystring = {"range": "6mo",
+                                   "interval": "1d"}
+                if '1 year' in time_parameter:
+                    querystring = {"range": "1y",
+                                   "interval": "1d"}
+                if '5 years' in time_parameter:
+                    querystring = {"range": "5y",
+                                   "interval": "1wk"}
+
+                if 'ytd' in time_parameter:
+                    querystring = {"range": "ytd",
+                                   "interval": "5d"}
+
+                response = requests.request("GET", yahoofinanceurlchart, headers=yahoofinanceapikey,
+                                            params=querystring)
+                response_json = json.loads(response.text)
+                dates_json = response_json['chart']['result'][0]['timestamp']
+                valuations = response_json['chart']['result'][0]['indicators']['quote'][0]['close']
+
+                def to_date(ts):
+                    return datetime.datetime.fromtimestamp(ts)
+
+
+                dates = list(map(to_date, dates_json))
+                plt.plot(dates, valuations)
+                ax = plt.gca()
+                ax.axes.xaxis.set_visible(False)
+                plt.show()
+
+                continue
 
         if 'news' in statement:
             speak('Searching news...')
             statement = statement.replace("news", "")
             top_headlines = newsapi_p.get_top_headlines(q=statement,
-                                                      category='business',
-                                                      language='en')
+                                                        category='business',
+                                                        language='en')
             if top_headlines['totalResults'] >= 3:
                 for news_id in range(0, 3):
                     news = top_headlines['articles'][news_id]
@@ -122,11 +172,3 @@ if __name__=='__main__':
                     news = top_headlines['articles'][news_id]
                     print(news['title'])
                     speak(news['title'])
-
-
-
-
-
-
-
-
